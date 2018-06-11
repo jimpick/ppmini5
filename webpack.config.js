e5a19ab3118bc8472c296654c6e868ae36b9fd7a
Jim Pick <jim@backblend.com>
2018-06-10 23:54:05 -0700

    Add support for electron
    server.config.js needed an extra option

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: ['babel-regenerator-runtime', './index.web.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'desktop', 'dist'),
+    publicPath: 'dist/',
+    filename: 'bundle.js',
+  },
+  node: {
+    __filename: true,
+    __dirname: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-electron',
+    },
+    extensions: ['.web.js', '.js', '.json'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(true),
+    }),
+  ],
+  target: 'electron-renderer',
+}
